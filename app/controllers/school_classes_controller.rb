ac90c3b33917aae3139f947b6279349e24c53cc1
Mohammed Noori <kmanoorib@gmail.com>
2019-06-24 03:02:00 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -29,7 +29,7 @@ class SchoolClassesController < ApplicationController
   
   private
   
-  def school_class_params
-    params.require(:school_class).permit!
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
   end
 end
