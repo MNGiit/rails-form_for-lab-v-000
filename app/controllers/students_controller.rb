2c9814d06acc18ab067c0bc2bfb29f38532e5bef
Mohammed Noori <kmanoorib@gmail.com>
2019-06-24 02:36:28 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,25 @@
+class StudentsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    
+    redirect_to student_path(@student)
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+end
